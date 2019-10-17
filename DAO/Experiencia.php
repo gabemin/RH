70afa4af8020ee0c2342c4dd79b9e170ead7b4ad
gabemin <g.sminetto94@gmail.com>
2019-10-17 11:12:43 -0400

    commit 17/10/2019
    CRUD da tabela experiências

--- DAO/Experiencia.php
+++ DAO/Experiencia.php
@@ -1,22 +1,42 @@
 <?php
-include_once ("../Controller/Connect.php");
+include_once("../Controller/Connect.php");
 
 class Experiencia
 {
     private $conn;
+
     public function __construct()
     {
-        $this->conn=new Connect();
-        $this->conn->setAttribute( PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $this->conn = new Connect();
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 }
 
-function create() {
-    //Todo
+function create($idPessoa, $empresa, $dataInicio, $dataFinal, $empregoAtual)
+{
+    $sql = "INSERT INTO experiencias(id_pessoa, empresa, dt_inicio, dt_fim, emprego_atual, dt_atualizacao)
+                                                                                        VALUES(?,?,?,?,?,?)";
+    $stmt = $sql->prepare($sql);
+    $stmt->execute([$idPessoa, $empresa, $dataInicio, $dataFinal, $empregoAtual, date('Y-m-d H:i:s.u')]);
 }
-function update(){
-    //Todo
+
+function update($empresa, $dataInicio, $dataFinal, $empregoAtual)
+{
+    $sql = "UPDATE experiencias SET empresa=?, dt_inicio=?, dt_fim=?, emprego_atual=?, dt_atualizacao=?";
+    $stmt = $sql->prepare($sql);
+    $stmt->execute([$empresa, $dataInicio, $dataFinal, date('Y-m-d H:i:s.u')]);
 }
-function delete(){
-    //Todo
+
+function delete($id)
+{
+    $sql = "DELETE * FROM experiencias WHERE id=?";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->execute([$id]);
+}
+
+function read($id){
+    $sql = "SELECT * FROM experiencias WHERE id_pessoa = ?";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->execute([$id]);
+    return $stmt->fetchAll();
 }
