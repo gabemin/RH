ed56d16c011a0aaade62487f4045ff6c5acba828
gabemin <g.sminetto94@gmail.com>
2019-10-21 12:27:54 -0400

    backend da adição de experiencia profissional.

--- Controller/novaExperiencia.php
+++ Controller/novaExperiencia.php
@@ -1,6 +1,12 @@
 <?php
 include_once ("../../../DAO/Experiencia.php");
+include_once ("../../../Model/Modal.php");
+session_start();
+$idPessoa = $_SESSION['id'];
+$experiencia = new Experiencia();
 
-if(isset($_POST[''])){
-
+if(isset($_POST['empresa'])){
+    $experiencia->create($idPessoa, $_POST['empresa'],$_POST['inicio'], $_POST['fim'], $_POST['descricaoCargo']);
+} else{
+    Modal::show('Erro', 'Algo deu errado');
 }
