0d8e70835d56ba32e2f9348d0df28318b2d084f5
gabemin <g.sminetto94@gmail.com>
2019-10-30 12:56:25 -0400

    pagina de cadastro de cursos

--- Controller/novoCurso.php
+++ Controller/novoCurso.php
@@ -1,1 +1,12 @@
 <?php
+include_once '../../../DAO/Curso.php';
+
+$curso = new Curso();
+
+if(isset($_POST['tipoCurso'])) {
+    try {
+        $curso->create();
+    } catch (PDOException $e) {
+        echo $e;
+    }
+}
--- DAO/Curso.php
+++ DAO/Curso.php
@@ -1,5 +1,5 @@
 <?php
-include_once "../Controller/Connect.php";
+include_once "../../../Controller/Connect.php";
 
 $conn;
 
